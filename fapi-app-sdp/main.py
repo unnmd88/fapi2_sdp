e1661a809b685de2eb20f9037d816ed0200f02dc
deb12 <unnmd88@gmail.com>
2025-09-11 18:55:43 +0300

    Create config settings

--- fapi-app-sdp/main.py
+++ fapi-app-sdp/main.py
@@ -1,9 +1,21 @@
 import uvicorn
 from fastapi import FastAPI
+from api.api_v1 import router as api_router
+
+from core.config import settings
 
 
 app = FastAPI()
+app.include_router(
+    api_router,
+    prefix='/api'
+)
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', reload=True, port=8181)
+    uvicorn.run(
+        'main:app',
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
