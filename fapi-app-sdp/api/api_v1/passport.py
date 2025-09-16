2def43665bc219d63065517690724163e1279cd6
auser <unnmd88@gmail.com>
2025-09-16 22:08:03 +0300

    Тест возврата файла в ответе upload_docx

--- fapi-app-sdp/api/api_v1/passport.py
+++ fapi-app-sdp/api/api_v1/passport.py
@@ -1,11 +1,16 @@
+import base64
+import io
 from collections.abc import Sequence
 
 import aiofiles
+import docx
 from fastapi import APIRouter
 from fastapi import Form, File, UploadFile, Request
-from starlette.responses import HTMLResponse
+from starlette.responses import HTMLResponse, Response
+from fastapi.responses import FileResponse
 
 from core.config import UPLOADS_URL
+from sdp_lib.passport.api import create_passport
 from utils.async_write_files import write_passport_docx
 
 
@@ -17,12 +22,25 @@ DIR_NAME = UPLOADS_URL / 'passport'
 async def upload_docx(
     err_color: str = Form(...),
     files: Sequence[UploadFile] = File(...),
-):
-    r = await write_passport_docx(*files)
-    return {
-        'files': len(files),
-        "Filenames": [file.filename for file in files],
-    }
+) :
+    passports = []
+    for src_file in files:
+        doc = docx.Document(src_file.file)
+        passp = create_passport(doc)
+        passp.get_docx().save('cccaddaaa.docx')
+    headers = {'Content-Disposition': 'attachment; filename="zagruzi_a.docx"'}
+    return FileResponse(
+        'cccaddaaa.docx', headers=headers
+    )
+    # for src_file in files:
+    #     doc = docx.Document(src_file.file)
+    #     fs = io.BytesIO()
+    #     doc.save(fs)
+    #     passports.append(fs)
+    #     fs.seek(0)
+
+
+
 
 
 @router.get("/")
