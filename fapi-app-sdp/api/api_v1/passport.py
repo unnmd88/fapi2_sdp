e9aab0e332e4d4358fc3d9baa08d5ff0ea33607b
deb12 <unnmd88@gmail.com>
2025-09-18 18:00:18 +0300

    PEP8

--- fapi-app-sdp/api/api_v1/passport.py
+++ fapi-app-sdp/api/api_v1/passport.py
@@ -2,44 +2,49 @@ from collections.abc import Sequence
 from fastapi.concurrency import run_in_threadpool
 
 from fastapi import APIRouter, HTTPException
-from fastapi import Form, File, UploadFile, Request
+from fastapi import File, UploadFile
 from starlette import status
-from starlette.responses import HTMLResponse, Response, StreamingResponse
 from fastapi.responses import FileResponse
 
 from docx import Document
 from core.config import UPLOADS_URL
 from sdp_lib.passport.api import create_passport
-from sdp_lib.passport.passport import Passport
-from sdp_lib.passport.utils import display_all_tables
-from utils.async_write_files import write_passport_docx
-from utils.files import save_and_zip_passport_docx, PassportSaver
+from utils.files import PassportSaver
 
 router = APIRouter(tags=['Passport'])
 DIR_NAME = UPLOADS_URL / 'passport'
 
 
-@router.post("/passport-validation")
+@router.post("/validation")
 async def upload_docx(
-    err_color: str = Form(...),
+    # err_color: str = Form(...),
     files: Sequence[UploadFile] = File(...),
 ) :
-    passports = [create_passport(src_file.filename, Document(src_file.file)) for src_file in files]
-    if not passports:
+    # passports = [create_passport(src_file.filename, Document(src_file.file)) for src_file in files]
+
+    if not files:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail='Не предоставлено ни одного файла для обработки.'
         )
+    allowed_files, bad_files = [], []
+    for f in files:
+        try:
+            doc = Document(f.file)
+            allowed_files.append(create_passport(f.filename, Document(f.file)))
+        except Exception:
+            bad_files.append(f)
+    if bad_files:
+        raise HTTPException(
+            400,
+            detail=f'Invalid document type. Files: {", ".join(f.filename for f in bad_files)}'
+        )
     # for f in files:
     #     display_all_tables(Document(f.file))
-    passport_saver = PassportSaver(passports)
+    passport_saver = PassportSaver(allowed_files)
     path = await run_in_threadpool(passport_saver.save_and_get_as_docx_if_only_one_else_as_zip_archive,)
     filename = 'result.docx' if path.suffix == ".docx" else path.name
     return FileResponse(path, headers={'Content-Disposition': f'attachment; filename={filename}'})
-
-
-
-
 
     # bstream.seek(0)
     # return StreamingResponse(
@@ -87,12 +92,3 @@ async def upload_docx(
 #     #     passports.append(fs)
 #     #     fs.seek(0)
 
-
-
-
-
-@router.get("/")
-async def main(request: Request):
-    return {
-        'res': 'hello from sdp-api'
-    }
